d98746ba527512fe038635db603f7b080398a994
Ad <adunreal@gmail.com>
2017-10-04 10:23:09 +0530

    Added functionality to store prices in local storage and then use those prices to display data

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+let storedData;
+let i = 0;
 var HttpClient = function() {
     this.get = function(aUrl, aCallback) {
         var anHttpRequest = new XMLHttpRequest();
@@ -22,23 +24,53 @@ client.get('https://bittrex.com/api/v1.1/public/getticker?market=btc-ok', functi
     chrome.browserAction.setBadgeText({text: String(last * 100000000)});
 });
 
+
 // Main Function
 setInterval(function(){
     // Get price
     client.get('https://bittrex.com/api/v1.1/public/getticker?market=btc-ok', function(response){
         // Parse JSON
-        var json = JSON.parse(response);
-        var result = json['result'];
+        const json = JSON.parse(response);
+        const result = json['result'];
         // Save it to DB
         // Check if price changed
-        current = result['Last'];
-        chrome.browserAction.setBadgeText({text: String(current * 100000000)});
+        current = result['Last'] * Math.pow(10,8);
+
+        chrome.browserAction.setBadgeText({text: String(current)});
             // If UP green, if DOWN red
             if (current > last) {
                 chrome.browserAction.setBadgeBackgroundColor({color: '#00cc00'});
             } else if (last > current) {
                 chrome.browserAction.setBadgeBackgroundColor({color: '#cc0000'});
             }
+
+        //Store recent 20 prices
+
+        chrome.storage.local.get(['prices'],(data)=>{
+
+            //Copy prices Array
+
+            let pricesArray = [].concat(data.prices);
+
+
+            //Check if pricesArray exists or not
+            if(!pricesArray) {
+                return null;
+            }
+
+            //Only store upto 20 values
+            if(pricesArray.length >= 20) {
+                pricesArray = pricesArray.slice(1,20);
+            }
+
+            pricesArray.push(current);
+
+            chrome.storage.local.set({prices: pricesArray}, ()=> {
+                console.log('Sucessfully saved pricesArray!');
+            })
+        });
+
+
 
         last = current;
     });
--- popup.js
+++ popup.js
@@ -1,47 +1,68 @@
-
-
-const fetchMarketHistory = ()=>{
-    /*
-        corsProxy is required
-    */
-
-    const corsProxy = 'https://cors-anywhere.herokuapp.com/'
-    const apiURL = 'https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-OK'
-
-    fetch(corsProxy+apiURL)
-    .then(response=>response.json())
-    .then(data=>{
-        const responseArray = data.result;
-        const resultsArray = responseArray.slice(0,10);
-
-        extractPrices(resultsArray);
-    })
-    .catch(err=>console.log(err))
-};
-
-const extractPrices = results=> {
-    const pricesArray = results.map(transcation=>transcation.Price)
-                        .map(value=>{
-                            const numStr = value.toString();
-                            return numStr;
-                        })
-                        .map(numStr=>numStr.padEnd(9,'0').slice(-4))
-                        .map(numStr=>{
-                            let num = parseFloat(numStr);
-                            num = (num/1000 <= 1) ? num*10 : num;
-                            num = num * 0.01;
-                            return num.toFixed(2);
-                        });
-    displayData(pricesArray);
-};
+//
+//
+// const fetchMarketHistory = ()=>{
+//     /*
+//         corsProxy is required
+//     */
+//
+//     const corsProxy = 'https://cors-anywhere.herokuapp.com/'
+//     const apiURL = 'https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-OK'
+//
+//     fetch(corsProxy+apiURL)
+//     .then(response=>response.json())
+//     .then(data=>{
+//         const responseArray = data.result;
+//         const resultsArray = responseArray.slice(0,10);
+//
+//         extractPrices(resultsArray);
+//     })
+//     .catch(err=>console.log(err))
+// };
+//
+// const extractPrices = results=> {
+//     const pricesArray = results.map(transcation=>transcation.Price)
+//                         .map(value=>{
+//                             const numStr = value.toString();
+//                             return numStr;
+//                         })
+//                         .map(numStr=>numStr.padEnd(9,'0').slice(-4))
+//                         .map(numStr=>{
+//                             let num = parseFloat(numStr);
+//                             num = (num/1000 <= 1) ? num*10 : num;
+//                             num = num * 0.01;
+//                             return num.toFixed(2);
+//                         });
+//     displayData(pricesArray);
+// };
 
 const displayData = data=>{
-    var graphData = {
-        labels: ['1','2','3','4','5','6','7','8','9','10'],
+    //Create labels for the graph depending upon number of items
+    const numberOfItems = data.length;
+    const labelsArray = Array.from(Array(numberOfItems).keys()).map(el=>el+1);
+
+
+    //Plot graphdata
+    const graphData = {
+        labels: labelsArray,
         series: [
             data
         ]
     };
+
+    //Draw the graph
     new Chartist.Line('.ct-chart', graphData);
 }
-document.addEventListener('DOMContentLoaded', fetchMarketHistory);
+
+const getMarketHistory = ()=>{
+    chrome.storage.local.get(['prices'],data=> {
+        const itemsArray = data.prices;
+        displayData(itemsArray);
+    });
+};
+
+const startExtension = ()=>{
+    getMarketHistory();
+    setInterval(getMarketHistory, 2000);
+};
+
+document.addEventListener('DOMContentLoaded', startExtension);
